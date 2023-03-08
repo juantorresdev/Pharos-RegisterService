cb0a635ceb53a874f416ca93556577a88bb332a8
juantorresdev <juan.torres.dev@gmail.com>
2023-03-07 23:43:50 -0600

    1 Proyect generated

---
+++ Register-service/src/main/java/com/pharos/service/register/RegisterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pharos.service.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterServiceApplication.class, args);
+	}
+
+}
